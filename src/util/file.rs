8d684587b90bcb9399b4630b84bf8f2be9a15301
scepter914 <scepter914@gmail.com>
2022-05-04 01:59:23 +0900

    move from divided crate
    Signed-off-by: scepter914 <scepter914@gmail.com>

---
+++ src/util/file.rs
@@ -0,0 +1,28 @@
+use chrono::Local;
+use std::path::{Path, PathBuf};
+
+/// Get string of time
+/// {year}-{month}-{day}-{hour}-{minutes}
+pub fn get_time_str() -> String {
+    Local::now().format("%Y%m%d_%H%M").to_string()
+}
+
+/// Get string of time with second.
+/// {year}-{month}-{day}-{hour}-{minutes}-{second}.
+pub fn get_time_str_with_sec() -> String {
+    Local::now().format("%Y%m%d_%H%M_%S").to_string()
+}
+
+/// Get string of time with millisecond.
+/// {year}-{month}-{day}-{hour}-{minutes}-{second}-{millisecond}
+pub fn get_time_str_with_ms() -> String {
+    Local::now().format("%Y%m%d_%H%M_%S_%3f").to_string()
+}
+
+/// Get time file path
+/// "{file_name}{now_time}.{extension}"
+pub fn get_time_filepath(file_name: impl Into<String>, extension: impl Into<String>) -> PathBuf {
+    let file_name_with_time = format!("{}{}", file_name.into(), get_time_str());
+    let file_with_extension = Path::new(&file_name_with_time).with_extension(extension.into());
+    file_with_extension
+}
---
+++ src/util/logger.rs
@@ -0,0 +1,49 @@
+use log::LevelFilter;
+use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
+use std::fs;
+use std::path::PathBuf;
+
+use crate::file;
+
+/// Logger class
+pub struct CarrotLogger {
+    /// The root result of directory
+    pub root_result_directory: PathBuf,
+}
+
+impl CarrotLogger {
+    /// Init for CarrotLogger
+    /// # Arguments
+    /// - log_directory_path: The log directory path
+    /// - log_file_level: The log level for file logger
+    /// - log_terninal_level: The log level for terninal logger
+    pub fn init(
+        log_directory_path: impl Into<PathBuf>,
+        log_file_level: LevelFilter,
+        log_terninal_level: LevelFilter,
+    ) -> CarrotLogger {
+        let root_result_directory = log_directory_path.into().join(file::get_time_str());
+        let _ = fs::create_dir(&root_result_directory);
+        let log_path = root_result_directory.join(file::get_time_filepath("log_", "txt"));
+
+        // logger init
+        CombinedLogger::init(vec![
+            TermLogger::new(
+                log_terninal_level,
+                Config::default(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            ),
+            simplelog::WriteLogger::new(
+                log_file_level,
+                Config::default(),
+                fs::File::create(log_path).unwrap(),
+            ),
+        ])
+        .unwrap();
+
+        CarrotLogger {
+            root_result_directory: root_result_directory.to_path_buf(),
+        }
+    }
+}
