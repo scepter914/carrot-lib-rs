de0946b3c98fa480f15c9c8a32c86ab67d73ffa2
scepter914 <scepter914@gmail.com>
2022-05-08 08:11:26 +0900

    add draw api
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- src/image/rgb_image/debug.rs
+++ src/image/rgb_image/debug.rs
@@ -1,5 +1,4 @@
 use image::{Rgb, RgbImage};
-use imageproc::drawing::draw_filled_circle;
 use imageproc::point::Point;
 
 /// Get image infomation
@@ -16,15 +15,4 @@ pub fn get_pixel_string_from_xy(image: &RgbImage, x: u32, y: u32) -> String {
 /// Get pixel information from imageproc::point::Point
 pub fn get_pixel_string(image: &RgbImage, point: Point<f32>) -> String {
     get_pixel_string_from_xy(image, point.x as u32, point.y as u32)
-}
-
-/// Get debug image drawing a point
-pub fn draw_point(image: &RgbImage, point: &Point<f32>, rgb: image::Rgb<u8>) -> RgbImage {
-    let point_size = image.width() / 80;
-    draw_filled_circle(
-        image,
-        (point.x as i32, point.y as i32),
-        point_size as i32,
-        rgb,
-    )
 }
---
+++ src/image/rgb_image/draw.rs
@@ -0,0 +1,27 @@
+use image::RgbImage;
+use imageproc::drawing;
+use imageproc::point::Point;
+use imageproc::rect::Rect;
+
+use crate::image::point;
+
+/// Get debug image drawing a point
+pub fn draw_point(image: &RgbImage, point: Point<f32>, rgb: image::Rgb<u8>) -> RgbImage {
+    let point_size: i32 = image.width() as i32 / 80;
+    drawing::draw_filled_circle(image, point::to_i32_tuple(point), point_size, rgb)
+}
+
+/// Get debug image drawing a point
+pub fn draw_rect(
+    image: &RgbImage,
+    center_point: Point<f32>,
+    height: u32,
+    width: u32,
+    rgb: image::Rgb<u8>,
+) -> RgbImage {
+    let x;
+    let y;
+    (x, y) = point::to_i32_tuple(center_point);
+    let rect = Rect::at(x, y).of_size(width, height);
+    drawing::draw_hollow_rect(image, rect, rgb)
+}
