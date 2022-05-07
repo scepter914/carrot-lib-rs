716e336da2b7889824613e643e29009544f79b3f
scepter914 <scepter914@gmail.com>
2022-05-07 16:54:37 +0900

    remove debug_message from debug function
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- src/image/rgb_image/debug.rs
+++ src/image/rgb_image/debug.rs
@@ -6,14 +6,11 @@ pub fn print_image_info(image: &RgbImage) -> String {
     format!("dimensions {:?}", image.dimensions())
 }
 
-pub fn print_pixel_from_point(debug_message: &str, image: &RgbImage, point: Point<f32>) -> String {
-    print_pixel(debug_message, image, point.x as u32, point.y as u32)
+pub fn print_pixel_from_point(image: &RgbImage, point: Point<f32>) -> String {
+    print_pixel(image, point.x as u32, point.y as u32)
 }
 
-pub fn print_pixel(debug_message: &str, image: &RgbImage, x: u32, y: u32) -> String {
+pub fn print_pixel(image: &RgbImage, x: u32, y: u32) -> String {
     let pixel: &Rgb<u8> = image.get_pixel(x, y);
-    format!(
-        "{}, RGB : {}, {}, {}",
-        debug_message, pixel[0], pixel[1], pixel[2]
-    )
+    format!("RGB : {}, {}, {}", pixel[0], pixel[1], pixel[2])
 }
