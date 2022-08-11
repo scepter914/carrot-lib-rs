f24ded181e3774337d640c03b2f94b0e3adcac73
scepter914 <scepter914@gmail.com>
2022-08-12 01:50:09 +0900

    fix path library
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- carrot_utils/src/path.rs
+++ carrot_utils/src/path.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 /// Get string of time with day.
 /// {year}{month}{day}.
 pub fn get_day_time() -> String {
-    Local::now().format("%Y%m%d_%H%M%S").to_string()
+    Local::now().format("%Y%m%d").to_string()
 }
 
 /// Get string of time with second.
@@ -23,8 +23,18 @@ pub fn get_ms_time() -> String {
 /// "TIME_aaa_bbb" ->  "20220101_010203_aaa_bbb"
 pub fn format_time_macro(string: String) -> PathBuf {
     let formatted_string: String = string
-        .replace("TIME_DAY", &get_day_time())
-        .replace("TIME_SEC", &get_sec_time())
-        .replace("TIME_MS", &get_ms_time());
+        .replace("{TIME_DAY}", &get_day_time())
+        .replace("{TIME_SEC}", &get_sec_time())
+        .replace("{TIME_MS}", &get_ms_time());
     PathBuf::from(&formatted_string)
+}
+
+#[test]
+fn test_path() {
+    println!("{}", get_day_time());
+    println!("{}", get_sec_time());
+    println!("{}", get_ms_time());
+    println!("{:?}", format_time_macro("test_{TIME_DAY}".to_string()));
+    println!("{:?}", format_time_macro("test_{TIME_SEC}".to_string()));
+    println!("{:?}", format_time_macro("test_{TIME_MS}".to_string()));
 }
