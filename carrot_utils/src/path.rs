7118a5263aeedad40fde21b72be1b6c55f22b481
scepter914 <scepter914@gmail.com>
2022-08-12 01:15:00 +0900

    update path library
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- carrot_utils/src/path.rs
+++ carrot_utils/src/path.rs
@@ -1,47 +1,30 @@
 use chrono::Local;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-/// Get string of time
-/// {year}-{month}-{day}-{hour}-{minutes}
-pub fn get_time_str() -> String {
-    Local::now().format("%Y%m%d_%H%M").to_string()
+/// Get string of time with day.
+/// {year}{month}{day}.
+pub fn get_day_time() -> String {
+    Local::now().format("%Y%m%d_%H%M%S").to_string()
 }
 
 /// Get string of time with second.
-/// {year}-{month}-{day}-{hour}-{minutes}-{second}.
-pub fn get_time_str_with_sec() -> String {
+/// {year}{month}{day}-{hour}{minutes}{second}.
+pub fn get_sec_time() -> String {
     Local::now().format("%Y%m%d_%H%M%S").to_string()
 }
 
 /// Get string of time with millisecond.
-/// {year}-{month}-{day}-{hour}-{minutes}-{second}-{millisecond}
-pub fn get_time_str_with_ms() -> String {
+/// {year}{month}{day}-{hour}{minutes}{second}-{millisecond}.
+pub fn get_ms_time() -> String {
     Local::now().format("%Y%m%d_%H%M%S_%3f").to_string()
 }
 
-/// Get time file path
-/// "{file_name}{now_time}.{extension}"
-pub fn get_time_filepath(file_name: impl Into<String>, extension: impl Into<String>) -> PathBuf {
-    let file_name_with_time = format!("{}{}", file_name.into(), get_time_str());
-    let file_with_extension = Path::new(&file_name_with_time).with_extension(extension.into());
-    file_with_extension
-}
-
-/// Get time file path with second.
-/// "{file_name}{now_time}.{extension}"
-pub fn get_time_filepath_sec(
-    file_name: impl Into<String>,
-    extension: impl Into<String>,
-) -> PathBuf {
-    let file_name_with_time = format!("{}{}", file_name.into(), get_time_str_with_sec());
-    let file_with_extension = Path::new(&file_name_with_time).with_extension(extension.into());
-    file_with_extension
-}
-
-/// Get time file path with millisecond.
-/// "{file_name}{now_time}.{extension}"
-pub fn get_time_filepath_ms(file_name: impl Into<String>, extension: impl Into<String>) -> PathBuf {
-    let file_name_with_time = format!("{}{}", file_name.into(), get_time_str_with_ms());
-    let file_with_extension = Path::new(&file_name_with_time).with_extension(extension.into());
-    file_with_extension
+/// Format time micro
+/// "TIME_aaa_bbb" ->  "20220101_010203_aaa_bbb"
+pub fn format_time_macro(string: String) -> PathBuf {
+    let formatted_string: String = string
+        .replace("TIME_DAY", &get_day_time())
+        .replace("TIME_SEC", &get_sec_time())
+        .replace("TIME_MS", &get_ms_time());
+    PathBuf::from(&formatted_string)
 }
