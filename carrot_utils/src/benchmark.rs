54a2ffaf3cd8b702b066b2326c543cb90062a7e4
scepter914 <scepter914@gmail.com>
2022-08-12 01:44:58 +0900

    update benchmark library
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- carrot_utils/src/benchmark.rs
+++ carrot_utils/src/benchmark.rs
@@ -1,11 +1,18 @@
 use std::time::Instant;
+use std::{thread, time};
 
 pub struct Benchmark {
     start_time: Instant,
 }
 
+impl Default for Benchmark {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Benchmark {
-    pub fn new() -> Benchmark {
+    pub fn new() -> Self {
         Benchmark {
             start_time: Instant::now(),
         }
@@ -15,12 +22,31 @@ impl Benchmark {
         self.start_time = Instant::now();
     }
 
-    pub fn get_bench_time(&self) -> String {
-        let end = self.start_time.elapsed();
-        format!(
-            "Process {}.{:03} msec",
-            end.as_micros() / 1000,
-            end.as_micros() % 1000,
-        )
+    pub fn stop(&self) -> String {
+        let end: time::Duration = self.start_time.elapsed();
+        if end.as_secs_f32() > 1.0 {
+            format!(
+                "{}.{:03} sec",
+                end.as_millis() / 1000,
+                end.as_millis() % 1000
+            )
+        } else {
+            format!(
+                "{}.{:03} ms",
+                end.as_micros() / 1000,
+                end.as_micros() % 1000,
+            )
+        }
     }
+}
+
+#[test]
+fn test_work_benchmark() {
+    let mut benchmark: Benchmark = Benchmark::new();
+    benchmark.start();
+    thread::sleep(time::Duration::from_millis(100));
+    println!("{}", benchmark.stop());
+    benchmark.start();
+    thread::sleep(time::Duration::from_millis(1500));
+    println!("{}", benchmark.stop());
 }
